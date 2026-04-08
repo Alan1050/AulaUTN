3b1cfe4434184ce7a25e17fdf7cdc895cbc1f928
MaxiUTNAY <tic-310196@utnay.edu.mx>
2026-04-08 15:37:43 -0700

    Corrección del icono de mostrar contraseña en login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -200,12 +200,12 @@ return (
                   <svg viewBox="0 0 24 24">
                     <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/>
                     <circle cx="12" cy="12" r="3"/>
-                    <line x1="3" y1="3" x2="21" y2="21"/>
                   </svg>
                 ) : (
-                  <svg viewBox="0 0 24 24">
+                   <svg viewBox="0 0 24 24">
                     <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/>
                     <circle cx="12" cy="12" r="3"/>
+                    <line x1="3" y1="3" x2="21" y2="21"/>
                   </svg>
                 )}
               </button>
